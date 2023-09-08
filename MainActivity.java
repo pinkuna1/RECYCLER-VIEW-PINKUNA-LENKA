69bc957aab9c2eeef4a0026be5917c5814eb0cae
pinkuna1 <144434032+pinkuna1@users.noreply.github.com>
2023-09-08 14:41:54 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ItemAdapter adapter;
+    private List<Item> itemList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        itemList = new ArrayList<>();
+        itemList.add(new Item("Item 1", "Item 1 Details"));
+        itemList.add(new Item("Item 2", "Item 2 Details"));
+        // Add more items as needed
+
+        adapter = new ItemAdapter(itemList, this);
+        recyclerView.setAdapter(adapter);
+    }
+}
