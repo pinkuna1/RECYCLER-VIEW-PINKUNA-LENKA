0463f0c82edcdc40a7f0fbf874f4852c868c3833
pinkuna1 <144434032+pinkuna1@users.noreply.github.com>
2023-09-08 14:38:54 +0530

    Create ITEM.java

---
+++ ITEM.java
@@ -0,0 +1,17 @@
+public class Item {
+    private String name;
+    private String details;
+
+    public Item(String name, String details) {
+        this.name = name;
+        this.details = details;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
